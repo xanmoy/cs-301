a9cd5ed36fb1fbf0ff12f233c61e60902fde875c
Tanmoy Ganguly <contact@xanmoy.me>
2024-06-07 21:37:54 +0530

    updated

--- classroom/classroom.sql
+++ classroom/classroom.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE college;
+USE college;
+CREATE TABLE student (
+	id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+    );
+    
+INSERT INTO student VALUES(1, "Tanmoy", 22);
+INSERT INTO student VALUES(2, "Xanmoy", 22);
 
+SELECT * FROM student;
