0ef7757d4f258d689e907a80df52a39d74734608
Tanmoy Ganguly <contact@xanmoy.me>
2024-06-07 21:50:45 +0530

    updated

--- classroom/classroom.sql
+++ classroom/classroom.sql
@@ -1,18 +1,9 @@
 CREATE DATABASE college;
-CREATE DATABASE IF NOT EXISTS college;
-DROP DATABASE IF EXISTS company;
 USE college;
-CREATE TABLE student (
-	id INT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT NOT NULL
-    );
-    
-INSERT INTO student VALUES(1, "Tanmoy", 22);
-INSERT INTO student VALUES(2, "Xanmoy", 22);
+
+CREATE TABLE student(
+	rollno INT PRIMARY KEY,
+    name Varchar(50)
+);
 
 SELECT * FROM student;
-
-SHOW DATABASES;
-
-SHOW TABLES;
