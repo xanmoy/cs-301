5629f917f09bc701e28293e2a94d63a0f00a4c63
Tanmoy Ganguly <contact@xanmoy.me>
2024-06-09 20:14:36 +0530

    updated

---
+++ classroom/ex.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE XYZ;
+
+USE XYZ;
+
+CREATE TABLE EMPLOYEE_INFO(
+  ID INT,
+  NAME VARCHAR(50),
+  SALARY INT
+);
+
+INSERT INTO EMPLOYEE_INFO VALUES (
+  1,
+  "adam",
+  25000
+),
+(
+  1,
+  "bob",
+  30000
+),
+(
+  1,
+  "casey",
+  40000
+);
+
+SELECT
+  *
+FROM
+  EMPLOYEE_INFO
