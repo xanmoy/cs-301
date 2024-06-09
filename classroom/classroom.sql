02dedbb5a7d1037a3bc2bb2a8cedc0be78301b3e
Tanmoy Ganguly <contact@xanmoy.me>
2024-06-10 04:05:34 +0530

    updated

--- classroom/classroom.sql
+++ classroom/classroom.sql
@@ -19,7 +19,7 @@ INSERT INTO student VALUES (104, "Shyam");
 
 create table emp(
 id int,
-salary int default 25000);
-
-insert into emp (id) values (101);
+salary int default 25000);/
+*
++-f/*sert into emp (id) values (101);
 select * from emp
